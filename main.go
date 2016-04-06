b37ec7079ed8665ef257ab28a30d4aa30813fa54
n.boughton <nicholasboughton@googlemail.com>
2016-04-06 10:33:03 +0100

    initial import

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jroimartin/gocui"
+)
+
+func main() {
+	// Initialise gui
+	g := gocui.NewGui()
+	if err := g.Init(); err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	// Set layout
+	g.SetLayout(layout)
+
+	// Assign keybindings
+	if err := keybindings(g); err != nil {
+		log.Panicln(err)
+	}
+
+	g.SelBgColor = gocui.ColorGreen
+	g.SelFgColor = gocui.ColorBlack
+	g.Cursor = true
+
+	// Call MainLoop to instantiate ui
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	// define starting size for main window (text output from server)
+	if v, err := g.SetView("main", -1, -1, maxX, maxY-2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "our text output appears here")
+	}
+
+	// define text entry screen
+	if v, err := g.SetView("input", -1, maxY-2, maxX, maxY); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Highlight = false
+		v.Editable = true
+		v.Wrap = true
+
+		if err := g.SetCurrentView("input"); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func keybindings(g *gocui.Gui) error {
+	if err := g.SetKeybinding("input", gocui.KeyCtrlQ, gocui.ModNone, quit); err != nil {
+		return err
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
